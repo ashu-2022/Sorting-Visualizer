805ce25604befe81151cbb24ebfc52aa3e7d3c93
ashutosh-pandit <ashutosh@dev.causalfunnel.com>
2024-02-28 10:48:53 +0530

    Sorting Visualzer version 1.0.0

---
+++ src/lib/types.ts
@@ -0,0 +1,11 @@
+export type SortingAlgorithmType = "bubble" | "insertion" | "selection" | "merge" | "quick"
+
+
+export type SelectOptionsType = {
+    label: string;
+    value: string;
+};
+  
+export type AnimateSpeedType = "slow" | "medium" | "fast" | "lighning";
+
+export type AnimationArrayType = [number[], boolean][];
